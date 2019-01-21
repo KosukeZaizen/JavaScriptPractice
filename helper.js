c37ca4f64c35bcbd1544663aa0a3fe68d5f90669
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2019-01-21 22:27:47 +0900

    dots are moving randomly

--- helper.js
+++ helper.js
@@ -5,15 +5,16 @@ Kosuke Zaizen
 */
 
 var curPage = "00";
+const objs = [];
 
 function initial(){
   changePage();
 }
 
+
 function setPage(num){
   curPage = num;
 }
-
 
 
 function changePage(){
@@ -34,11 +35,39 @@ function gebId(id){
   return document.getElementById(id);
 }
 
+
+function gebClass(strClass){
+  return document.getElementsByClassName(strClass);
+}
+
+
 function hide(obj){
   obj.style.display="none";
 }
 
+
 function show(obj){
   obj.style.display="block";
+}
+
+
+function addObj(obj){
+  objs.push(obj);
+}
+
+
+function createChar(char, strClass, x, y){
+
+  var span = document.createElement('span');
+
+  span.innerHTML = char;
+  span.className = strClass;
+  span.style.position = "fixed";
+  span.style.top = y + "px";
+  span.style.left = x + "px";
+
+  gebId("page" + curPage).appendChild(span);
+
+  addObj(span)
 }
 
--- main.js
+++ main.js
@@ -4,9 +4,31 @@ create 2019/01/21
 Kosuke Zaizen
 */
 
+
 function startGame(){
   setPage("01");
   changePage();
+
+  for(i=0; i<100; i++){
+    for(j=0; j<100; j++){
+      createChar(".", "particle", window.innerWidth/2, window.innerHeight/2);
+    }
+  }
+  setInterval("nextTime()",10);
 }
 
+function nextTime(){
+  for (i = 0; i < objs.length; i++){
+    var rand;
+    obj = objs[i];
+    if(obj.className == "particle"){
+
+      rand = Math.floor( Math.random() * 7 ) - 3;
+      obj.style.top = parseInt(obj.style.top) + rand + "px";
+
+      var rand = Math.floor( Math.random() * 7 ) - 3;
+      obj.style.left = parseInt(obj.style.left) + rand + "px";
+    }
+  }
+}
 
