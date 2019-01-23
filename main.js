3a73d3ba3ab1b2ea699778d1d053bc24ae3ca6c5
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2019-01-23 11:52:21 +0900

    change name

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ create 2019/01/21
 Kosuke Zaizen
 */
 
-intTimeStep = 10;
+intTimeStep = 2;
 
 function startGame(){
   setPage("01");
@@ -15,15 +15,15 @@ function startGame(){
   //---------dots---------
   for(i=wCenter-40; i<wCenter+40; i++){
     for(j=hCenter-25; j<hCenter+25; j++){
-      createChar(".", "particle", i, j, 100);
+      createChar(".", "particle", i, j, 10);
     }
   }
 
   makeMethodForEachTime("particle",
 
     function(){
-      randX = Math.floor( Math.random() * 3 ) - 1;
-      randY = Math.floor( Math.random() * 3 ) - 1;
+      randX = Math.floor( Math.random() * 5 ) - 2;
+      randY = Math.floor( Math.random() * 5 ) - 2;
 
       this.addForce(randX, randY);
     }
@@ -32,42 +32,30 @@ function startGame(){
 
 
   //---------puff---------
-  createImg("image/syuu.png", "syuu", wCenter, -200, 300);
+  createImg("image/syuu.png", "syuu", wCenter, -200, 100);
 
   makeMethodForEachTime("syuu",
 
     function(){
-      this.addForce(0, 10);
+      this.addForce(0, 50);
     }
   );
 
 
 
   //---------fish---------
-  for(i=-50; i<50; i++){
-    createImg("image/fish.png", "fish", -500 + i, hCenter + i, 300);
+  for(i=-2; i<2; i++){
+    createImg("image/fish.png", "fish", -500 + i, hCenter + i, 30);
   }
-  for(i=-50; i<50; i++){
-    createImg("image/fish.png", "fish", -3500 + i, hCenter + i, 300);
-  }
-  for(i=-50; i<50; i++){
-    createImg("image/fish.png", "fish", -10000 + i, hCenter + i, 300);
-  }
-  for(i=-100; i<100; i++){
-    createImg("image/fish.png", "fish", -15000 + i, hCenter + i, 300);
-  }
-  for(i=-100; i<100; i++){
-    createImg("image/fish.png", "fish", -15000 + i, hCenter + i, 300);
-  }
-  for(i=-100; i<100; i++){
-    createImg("image/fish.png", "fish", -15000 + i, hCenter + i, 300);
+  for(i=-2; i<2; i++){
+    createImg("image/fish.png", "fish", -3500 + i, hCenter + i, 30);
   }
 
 
   makeMethodForEachTime("fish",
 
     function(){
-      randX = Math.floor( Math.random() * 11 ) - 4;
+      randX = Math.floor( Math.random() * 31 ) - 14;
       randY = Math.floor( Math.random() * 5 ) - 2;
 
       this.addForce(randX, randY);
